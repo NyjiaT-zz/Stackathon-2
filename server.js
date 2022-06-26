be516e1d74767418660c33fe3f9889b766875244
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-26 17:36:39 -0500

    created room id

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
-const express = require('express')
-;
+const express = require('express');
+//for unique room id
+const {v4: uuidv4} = require('uuid');
 const app = express();
-const server = require('http').Server(app)
+const server = require('http').Server(app);
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
 
+
+// main route generates uuid for new user and redirects to that room
 app.get('/', (req, res, next) => {
   try {
-    res.render('room')
+    res.redirect(`/${uuidv4()}`)
   }
   catch(err) {
     console.log(err)
   }
 })
+
+app.get('/:room', (req, res, next) => {
+  res.render('room', { roomId: req.params.room })
+})
+
 
 server.listen(3030);
