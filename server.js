996ab99f25e2ccee4889e301dda857986c29f064
Nyjia Williams <nyjiawilliams@gmail.com>
2022-06-26 17:25:03 -0500

    initialized first view

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require('express')
 const app = express();
 const server = require('http').Server(app)
 
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res, next) => {
   try {
-    res.status(200).send('Chatting on port 3030')
+    res.render('room')
   }
   catch(err) {
     console.log(err)
